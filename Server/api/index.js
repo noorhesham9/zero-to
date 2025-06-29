c1002c5d2f030bbe77f17b8e43f65e2909ab8c59
noor hesham <norhsham02@gmail.com>
2025-06-29 05:09:46 +0300

    edit

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,7 +1,24 @@
-const { createServer } = require("http");
 const app = require("../app");
+const mongoose = require("mongoose");
 
-module.exports = (req, res) => {
-  const server = createServer(app);
-  server.emit("request", req, res);
+let cachedConn = null;
+
+module.exports = async (req, res) => {
+  try {
+    if (!cachedConn) {
+      await mongoose.connect(process.env.CONN_STR, {
+        dbName:
+          process.env.NODE_ENV === "production"
+            ? "CircleEye_Production"
+            : "CircleEye_Dev",
+      });
+      console.log("MongoDB connected in serverless");
+      cachedConn = mongoose.connection;
+    }
+
+    app(req, res);
+  } catch (error) {
+    console.error("API error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
